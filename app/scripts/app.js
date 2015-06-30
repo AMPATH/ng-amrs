54d5a5c4f8bbc84e677cc7ad03f002b8a8484f8d
nkimaina <nkimaina@ampath.or.ke>
2015-06-30 14:25:56 +0300

    Added ui router

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -15,9 +15,45 @@ angular
     'ngResource',
     'ngRoute',
     'ngSanitize',
-    'ngTouch'
+    'ngTouch',
+    'ui.router'
   ])
-  .config(function ($routeProvider) {
+    .config(function ($stateProvider, $urlRouterProvider) {
+    $urlRouterProvider.otherwise('/');
+    $stateProvider
+      .state('home', {
+        url: '/',
+        templateUrl: 'views/appDashboard/main.html',
+        controller:'MainCtrl',
+        data : {requireLogin : true }
+      })
+      .state('about', {
+        url: '/about',
+        templateUrl: 'views/appDashboard/about.html',
+        controller: 'AboutCtrl',
+        data : {requireLogin : true }
+      });
+
+  }).run(function ($rootScope, $state, $location) {
+
+  $rootScope.$on('$stateChangeStart', function (event, toState, toParams) {
+
+ /*   var shouldLogin = toState.data !== undefined && toState.data.requireLogin && !Authentication.isAuthenticated ;
+
+    if(shouldLogin)
+    {
+      $state.go('login',{onSuccessRout:toState, onSuccessParams:toParams});
+      event.preventDefault();
+      return;
+    }
+    //else navigate to page 
+    */
+
+  });
+});
+
+  
+  /*.config(function ($routeProvider) {
     $routeProvider
       .when('/', {
         templateUrl: 'views/appDashboard/main.html',
@@ -30,4 +66,4 @@ angular
       .otherwise({
         redirectTo: '/'
       });
-  });
+  });*/
