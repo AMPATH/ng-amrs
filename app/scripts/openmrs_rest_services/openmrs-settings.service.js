c128aabef8e732d87f6d5353a93ff349f865f11d
nkimaina <nkimaina@ampath.com>
2015-07-02 15:20:38 +0300

    AA-29: added the openmrs settings file to store urlbase and the likes

---
+++ app/scripts/openmrs_rest_services/openmrs-settings.service.js
@@ -0,0 +1,31 @@
+(function() {
+    'use strict';
+
+    angular
+        .module('OpenMRS_RestServices')
+        .service('OpenMRS_Settings', Service);
+
+    Service.$inject = [];
+
+    function Service() {
+        var serviceDefinition;
+        var restUrlBaseList = ['https://test1.ampath.or.ke:8443/amrs/ws/rest/v1/'];
+        var restUrlBase = restUrlBaseList[0];
+
+        serviceDefinition = {
+          getCurrentRestUrlBase:getCurrentRestUrlBase,
+          setCurrentRestUrlBase:setCurrentRestUrlBase,
+          restUrlBase:restUrlBase
+        };
+        return serviceDefinition;
+
+        function getCurrentRestUrlBase() {
+          return restUrlBase;
+        }
+
+        function setCurrentRestUrlBase(url) {
+          restUrlBase = url;
+        }
+
+    }
+})();
