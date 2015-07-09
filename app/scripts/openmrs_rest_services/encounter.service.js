4096647154e2c6788dfbf5e57d6352d8acd7e98b
mhawila <a.mhawila@gmail.com>
2015-07-09 13:37:02 -0400

    AA-30: Added saveEncounter functionality to encounter service

--- app/scripts/openmrs_rest_services/encounter.service.js
+++ app/scripts/openmrs_rest_services/encounter.service.js
@@ -7,20 +7,33 @@
   function EncounterService(Restangular) {
     var _service = this;
     _service = getEncounterByUuid;
+    _service = saveEncounter;
 
-    function getEncounterByUuid(uuid, callback) {
-      Restangular.one('encounter',uuid).get().then(function(data) {
-        if (typeof callback !== 'function') {
+    function getEncounterByUuid(uuid, successCallback, errorCallback) {
+      Restangular.one('encounter', uuid).get().then(function(data) {
+        if (typeof successCallback !== 'function') {
           console.log('Error: You need a callback function to process' +
           ' results');
           return;
         }
 
-        callback(data);
+        successCallback(data);
       },
       function(error) {
         console.log('An error occured while attempting to fetch encounter ' +
                     'with uuid ' + uuid);
+        if (typeof errorCallback === 'function') errorCallback(error);
+      });
+    }
+
+    function saveEncounter(encounter, successCallback, errorCallback) {
+      Restangular.service('encounter').post(encounter).then(function(success) {
+        console.log('Encounter saved successfully');
+        if (typeof successCallback === 'function') successCallback(success);
+      },
+      function(error) {
+        console.log('Sorry buddy, an error occured while saving encounter');
+        if (typeof errorCallback === 'function') errorCallback(error);
       });
     }
   }
