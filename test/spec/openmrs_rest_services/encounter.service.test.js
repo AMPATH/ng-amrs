95c97724dcd5eb867d96f4da8b024a6e95359de2
mhawila <a.mhawila@gmail.com>
2015-07-24 09:54:33 +0300

    AA-30: Added a method to retrieve patient encounters given patient uuid

--- test/spec/openmrs_rest_services/encounter.service.test.js
+++ test/spec/openmrs_rest_services/encounter.service.test.js
@@ -6,9 +6,10 @@
         module('OpenmrsRestServices');
       });
 
-      var baseURl = 'https://etl1.ampath.or.ke:8443/amrs/ws/rest/v1/';
-      var httpBackend
+      var testRestUrl = 'http://testingurl/openmrs/ws/rest/v1/';
+      var httpBackend;
       var encounterService;
+      var OpenmrsSettings;
       
       var dummyEncounter = {
         uuid:'encounter-test-uuid',
@@ -20,24 +21,56 @@
       beforeEach(inject(function($injector) {
         httpBackend = $injector.get('$httpBackend');
         encounterService = $injector.get('EncounterService');
+        OpenmrsSettings = $injector.get('OpenmrsSettings');
+
+        //  Set restangular URL
+        $injector.get('Restangular').setBaseUrl(testRestUrl);
       }));
 
       afterEach (function() {
         httpBackend.verifyNoOutstandingExpectation ();
 
-        //httpBackend.verifyNoOutstandingRequest (); expectation is sufficient for now
+        //  httpBackend.verifyNoOutstandingRequest (); expectation is sufficient for now
       });
       
       it('Should have encounter service defined', function(){
         expect(encounterService).to.exists;
       });
       
-      it('Should call the rest end point when getEncounter is called', function() {
-        httpBackend.expectGET(baseURl + 'encounter/encounter-test-uuid').respond(dummyEncounter);
-        httpBackend.flush();
+      it('Should call the appropriate rest end point when getEncounter is called', function() {
+        httpBackend.expectGET(testRestUrl + 'encounter/encounter-test-uuid').respond(dummyEncounter);
         encounterService.getEncounterByUuid('encounter-test-uuid', function(data){
           expect(data.uuid).to.equal(dummyEncounter.uuid);
-        }));
+        });
+        httpBackend.flush();
+      });
+      
+      it('Should save new Encounter to the database', function() {
+        
+      });
+      
+      it('Should make a call to retrieve a list of encounters for a patient', function (){
+        var response = { "results" :[
+                          {
+                            "uuid": "encounter-uuid-for-first-element",
+                            "display": "ADULTRETURN 01/02/2006",
+                          },
+                          {
+                            "uuid": "bf218490-1691-11df-97a5-7038c432aabf",
+                            "display": "ADULTRETURN 07/02/2006",
+                            "links": [
+                              {
+                                "uri": testRestUrl+"encounter/bf218490-1691-11df-97a5-7038c432aabf",
+                                "rel": "self"
+                              }
+                            ]
+                          }
+                      ]};
+          httpBackend.expectGET(testRestUrl + 'encounter?patient=patient-uuid').respond(response);
+          encounterService.getPatientEncounters('patient-uuid',function(data){
+            expect(data[0].uuid).to.equal('encounter-uuid-for-first-element');
+          },function(error){console.log('Error')});
+          httpBackend.flush();
       });
   });
 })();
