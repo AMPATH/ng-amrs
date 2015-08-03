7601b38914c293675646c595d409527858cc776a
nkimaina <nkimaina@ampath.or.ke>
2015-08-03 11:43:21 +0300

    AA-17: Added directive for hiv summary and added section to patient-dashboard

---
+++ app/scripts/patient-dashboard/directives/hiv-summary.directive.js
@@ -0,0 +1,51 @@
+/*
+jshint -W003, -W026
+*/
+(function () {
+    'use strict';
+
+    angular
+        .module('ngAmrsApp')
+        .directive('hivSummary', hivSummary);
+
+    function hivSummary() {
+        var directive = {
+            restrict: "E",
+            scope: { patientUuid: "@" },
+            controller: hivSummaryController,
+            link: hivSummaryLink,
+            templateUrl: "views/patient-dashboard/hiv-summary-pane.html",
+        };
+
+        return directive;
+    }
+    
+    hivSummaryController.$inject = ['$scope', 'EtlRestService'];
+    
+    function hivSummaryController(scope, EtlRestService) {
+        var vm = this;
+        scope.hivSummary = [];
+        
+        vm.fetchHivSummary = function(patientUuid) {
+            EtlRestService.getHivSummary(patientUuid, undefined, undefined, onFetchHivSummarySuccess, onFetchHivSummaryFailed);
+        }
+        
+        function onFetchHivSummarySuccess(hivData) {
+            scope.hivSummary = hivData.result[0];
+        }
+        
+        function onFetchHivSummaryFailed(error) {
+           scope.hivSummary = [];
+        }
+    }
+
+    function hivSummaryLink(scope, element, attrs, vm) {
+        attrs.$observe('patientUuid', onPatientUuidChanged);
+
+        function onPatientUuidChanged(newVal, oldVal) {
+            if (newVal && newVal != "") {
+                vm.fetchHivSummary(newVal);
+            }
+        }
+    }
+})();
