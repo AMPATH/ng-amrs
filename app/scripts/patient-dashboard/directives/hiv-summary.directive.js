75b958d70f1706d2d7d0b905c550ec98eba14749
nkimaina <nkimaina@ampath.or.ke>
2015-08-06 11:13:22 +0300

    AA-17: Added message when summary not available

--- app/scripts/patient-dashboard/directives/hiv-summary.directive.js
+++ app/scripts/patient-dashboard/directives/hiv-summary.directive.js
@@ -26,16 +26,23 @@ jshint -W003, -W026
         var vm = this;
         scope.hivSummary = {};
         
+        scope.hasSummary = false;
+        
         vm.fetchHivSummary = function(patientUuid) {
+            scope.hasSummary = false;
             EtlRestService.getHivSummary(patientUuid, undefined, undefined, onFetchHivSummarySuccess, onFetchHivSummaryFailed);
         }
         
         function onFetchHivSummarySuccess(hivData) {
+            if(hivData.result[0])
+                scope.hasSummary = true;
+                
             scope.hivSummary = new HivSummaryModel.hivSummary(hivData.result[0]);
         }
         
         function onFetchHivSummaryFailed(error) {
-           scope.hivSummary = {};
+            scope.hasSummary = false;
+            scope.hivSummary = {};
         }
     }
 
