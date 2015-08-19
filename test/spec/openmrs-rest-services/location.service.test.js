66a514ddc38c475fbac4c0b4fbe9af99d1702848
nkimaina <nkimaina@ampath.or.ke>
2015-08-19 11:55:28 +0300

    AA-86: Added method to fetch all locations and to cache them in memory in the location res service

--- test/spec/openmrs-rest-services/location.service.test.js
+++ test/spec/openmrs-rest-services/location.service.test.js
@@ -1,3 +1,9 @@
+/* global afterEach */
+/* global describe */
+/* global inject */
+/* global beforeEach */
+/* global expect */
+/* global it */
 /*jshint -W026, -W030 */
 (function () {
   'use strict';
@@ -12,14 +18,14 @@
     var httpBackend;
     var locationService;
     var settingsService;
-
+    var testLocations;
     beforeEach(inject(function ($injector) {
       httpBackend = $injector.get('$httpBackend');
       locationService = $injector.get('LocationResService');
       settingsService = $injector.get('OpenmrsSettings');
     }));
 
-    beforeEach(inject(function() {
+    beforeEach(inject(function () {
       callbacks = {
         onSuccessCalled: false,
         onFailedCalled: false,
@@ -33,6 +39,25 @@
           callbacks.message = message;
         }
       };
+
+      testLocations = [
+        {
+          uuid: '08feae7c-1352-11df-a1f1-0026b9348838',
+          name: 'Location-1',
+          display: 'Location-1'
+        },
+        {
+          uuid: '00b47ef5-a29b-40a2-a7f4-6851df8d6532',
+          name: 'Location-100',
+          display: 'Location-100'
+        },
+        {
+          uuid: '9fcf21c-8a00-44ba-9555-dde4dd877c4a',
+          name: 'Location-101',
+          display: 'Location-101'
+        }
+      ];
+
     }));
 
     afterEach(function () {
@@ -57,9 +82,39 @@
       httpBackend.flush();
     });
 
+    it('should make an api call to the location resource when getLocations is called with refreshCache param being true', function () {
+      httpBackend.expectGET(settingsService.getCurrentRestUrlBase() + 'location?v=default').respond({});
+      locationService.getLocations(function () { }, function () { }, true);
+      httpBackend.flush();
+    });
+
+    it('should make an api call to the location resource when getLocations is called with refreshCache param being false and there are no cachedLocations', function () {
+      locationService.cachedLocations = []; //clear cached locations
+      
+      httpBackend.expectGET(settingsService.getCurrentRestUrlBase() + 'location?v=default').respond({});
+      locationService.getLocations(function () { }, function () { }, false);
+      httpBackend.flush();
+    });
+
+    it('should not make an api call to the location resource when getLocations is called with refreshCache param being false and there are cachedLocations', function () {
+      locationService.cachedLocations = testLocations; //set cachedLocations 
+      var returnedLocations = [];
+      locationService.getLocations(function (locations) { returnedLocations = locations; }, function () { }, false);
+      expect(returnedLocations).to.deep.equal(testLocations);
+    });
+
+    it('should set the cachedLocations when getLocations is called with refreshCache param being true', function () {
+      locationService.cachedLocations = []; //reset cachedLocations 
+      
+      httpBackend.expectGET(settingsService.getCurrentRestUrlBase() + 'location?v=default').respond({ results: testLocations });
+      locationService.getLocations(function () { }, function () { }, true);
+      httpBackend.flush();
+      expect(locationService.cachedLocations).to.deep.equal(testLocations);
+    });
+
     it('should call the onSuccess callback getLocationByUuid request successfully returns', function () {
       httpBackend.expectGET(settingsService.getCurrentRestUrlBase() + 'location/passed-uuid').respond({});
-      locationService.getLocationByUuid('passed-uuid',callbacks.onSuccess, callbacks.onFailure);
+      locationService.getLocationByUuid('passed-uuid', callbacks.onSuccess, callbacks.onFailure);
       httpBackend.flush();
       expect(callbacks.onSuccessCalled).to.equal(true);
       expect(callbacks.onFailedCalled).to.equal(false);
@@ -67,17 +122,17 @@
 
     it('should call the onFailed callback when getLocationByUuid request is not successfull', function () {
       httpBackend.expectGET(settingsService.getCurrentRestUrlBase() + 'location/passed-uuid').respond(500);
-      locationService.getLocationByUuid('passed-uuid',callbacks.onSuccess, callbacks.onFailure);
+      locationService.getLocationByUuid('passed-uuid', callbacks.onSuccess, callbacks.onFailure);
       httpBackend.flush();
       expect(callbacks.onSuccessCalled).to.equal(false);
       expect(callbacks.onFailedCalled).to.equal(true);
       expect(callbacks.message).to.exist;
       expect(callbacks.message.trim()).not.to.equal('');
     });
-    
-        it('should call the onSuccess callback findLocation request successfully returns', function () {
+
+    it('should call the onSuccess callback findLocation request successfully returns', function () {
       httpBackend.expectGET(settingsService.getCurrentRestUrlBase() + 'location?q=passed-text&v=default').respond({});
-      locationService.findLocation('passed-text',callbacks.onSuccess, callbacks.onFailure);
+      locationService.findLocation('passed-text', callbacks.onSuccess, callbacks.onFailure);
       httpBackend.flush();
       expect(callbacks.onSuccessCalled).to.equal(true);
       expect(callbacks.onFailedCalled).to.equal(false);
@@ -85,7 +140,25 @@
 
     it('should call the onFailed callback when findLocation request is not successfull', function () {
       httpBackend.expectGET(settingsService.getCurrentRestUrlBase() + 'location?q=passed-text&v=default').respond(500);
-      locationService.findLocation('passed-text',callbacks.onSuccess, callbacks.onFailure);
+      locationService.findLocation('passed-text', callbacks.onSuccess, callbacks.onFailure);
+      httpBackend.flush();
+      expect(callbacks.onSuccessCalled).to.equal(false);
+      expect(callbacks.onFailedCalled).to.equal(true);
+      expect(callbacks.message).to.exist;
+      expect(callbacks.message.trim()).not.to.equal('');
+    });
+
+    it('should call the onSuccess callback getLocations request successfully returns', function () {
+      httpBackend.expectGET(settingsService.getCurrentRestUrlBase() + 'location?v=default').respond({});
+      locationService.getLocations(callbacks.onSuccess, callbacks.onFailure, true);
+      httpBackend.flush();
+      expect(callbacks.onSuccessCalled).to.equal(true);
+      expect(callbacks.onFailedCalled).to.equal(false);
+    });
+
+    it('should call the onFailed callback when getLocations request is not successfull', function () {
+      httpBackend.expectGET(settingsService.getCurrentRestUrlBase() + 'location?v=default').respond(500);
+      locationService.getLocations(callbacks.onSuccess, callbacks.onFailure, true);
       httpBackend.flush();
       expect(callbacks.onSuccessCalled).to.equal(false);
       expect(callbacks.onFailedCalled).to.equal(true);
