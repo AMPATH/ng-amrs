1254cad924cf80df0b666b6383e8840f080885b4
nkimaina <nkimaina@ampath.or.ke>
2015-08-27 11:55:11 +0300

    AA-87: Added unit tests for clinic dashboard controller

---
+++ test/spec/clinic-dashboard/controllers/clinic-dashboard.controller.test.js
@@ -0,0 +1,72 @@
+/*
+jshint -W098, -W117, -W030
+*/
+(function () {
+	'use strict';
+	describe('Controller: ClinicDashboardCtrl', function () {
+		// body...
+		var controller;
+		var stateParams;
+		var OpenmrsRestService;
+		var controllerScope;
+		var locationModel;
+		var locationService;
+
+		beforeEach(function () {
+			module('app.clinicDashboard');
+			module('models');
+			module('mock.openmrsRestServices');
+		});
+
+		beforeEach(module('ui.router')); //to enable using stateparams
+
+		beforeEach(inject(function ($controller, $injector, _$stateParams_, $rootScope) {
+			/*
+			When testing controllers it not wise to inject them as it is done when testing services
+			It is advisable to use $controller to instantiate instead of using the $injector service
+			*/
+
+			OpenmrsRestService = $injector.get('OpenmrsRestService');
+			locationService = OpenmrsRestService.getLocationResService();
+			locationModel = $injector.get('LocationModel'); 
+			controllerScope = $rootScope.$new();
+			stateParams = _$stateParams_;
+			controller = $controller('ClinicDashboardCtrl', { $rootScope: $rootScope, $scope: controllerScope, $stateParams: stateParams, OpenmrsRestService: OpenmrsRestService, LocationModel: locationModel });
+
+		}));
+
+		it('ClinicDashboardCtrl controller should have all Injected Services', function () {
+			expect(controller).to.exist;
+			
+			//ensure that mock services are injected
+			expect(OpenmrsRestService.isMockService).to.equal(true);
+			expect(locationService.isMockService).to.equal(true);
+		});
+		
+		it('should call the locations service getlocations method on load to fetch locations', function () {
+			//this should have been called when the controller was initalizing 
+			expect(locationService.getLocationsCalled).to.equal(true);
+		});
+		
+		it('should switch the active tab by changing the activeTabId when switchTabByIndex is invoked', function () {
+			controllerScope.switchTabByIndex(1);
+			expect(controllerScope.activeTabId).to.equal(1);
+			
+			controllerScope.switchTabByIndex(2);
+			expect(controllerScope.activeTabId).to.equal(2);
+		});
+		
+		it('should should wrap received locations and assign it to the scope when fetchLocations is called', function () {
+			//fetchLocations is called when the controller was initalizing 
+			expect(controllerScope.locations.length).to.equal(locationService.getMockLocations().length);
+			
+			//check for wrapping 
+			var firstLocation = locationService.getMockLocations()[0];
+			var wrappedLocation = locationModel.toWrapper(firstLocation);
+			
+			expect(controllerScope.locations[0].uuId()).to.equal(wrappedLocation.uuId());
+			
+		});
+
+	});
+})();
