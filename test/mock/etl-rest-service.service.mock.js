7faf3ca1b44fc86e8dfc4075815e60fdb206ae0a
nkimaina <nkimaina@ampath.or.ke>
2015-08-31 16:06:59 +0300

    Unit-Tests: added the appointment schedule directive unit tests

--- test/mock/etl-rest-service.service.mock.js
+++ test/mock/etl-rest-service.service.mock.js
@@ -14,10 +14,16 @@ jshint -W098, -W117, -W003, -W026
     var numberOfPatientTestsToReturn = 40;
     var returnErrorOnNextCall = false;
     var service = {
+      isMockService: true,
       getVitals: getVitals,
-      getPatientTests: getPatientTests,
       numberOfVitalsToReturn: numberOfVitalsToReturn,
+      
+      getPatientTests: getPatientTests,
       numberOfPatientTestsToReturn: numberOfPatientTestsToReturn,
+      
+      getAppointmentSchedule:getAppointmentSchedule,
+      numberOfAppointmentsToReturn: 20,
+      
       returnErrorOnNextCall: returnErrorOnNextCall
     };
     //debugger;
@@ -115,6 +121,52 @@ jshint -W098, -W117, -W003, -W026
 
     }
 
+    function getAppointmentSchedule(locationUuid, startDate, endDate, successCallback, failedCallback, startIndex, limit) {
+      if (!startIndex) {
+        startIndex = 0;
+      }
+
+      if (!limit) {
+        limit = 20;
+      }
+      if (service.returnErrorOnNextCall === true) {
+        console.log('returning error on get vitals');
+        failedCallback({ message: 'An error occured' });
+        return;
+      }
+
+      var appointments = [];
+
+      var numberOfRecords = limit;
+
+      if (startIndex >= service.numberOfAppointmentsToReturn) {
+        successCallback({
+          startIndex: startIndex,
+          size: 0,
+          result: []
+        });
+        return;
+      }
+
+      if ((startIndex + limit) > service.numberOfAppointmentsToReturn) {
+        numberOfRecords = service.numberOfAppointmentsToReturn - (startIndex + limit);
+      }
+      else {
+        numberOfRecords = limit;
+      }
+
+      for (var i = startIndex; i < (startIndex + numberOfRecords); i++) {
+        appointments.push(getAppointmentScheduleRecord(i));
+      }
+
+      successCallback({
+        startIndex: startIndex,
+        size: numberOfRecords,
+        result: appointments
+      });
+
+    }
+
     function getVitalRecord(index) {
       var vitalRecord = {
         person_id: 'person_id',
@@ -153,6 +205,59 @@ jshint -W098, -W117, -W003, -W026
       };
       return patientTestEtl;
     }
+
+    function getAppointmentScheduleRecord(index) {
+      /* jshint ignore:start */
+      var appointmentScheduleEtl = {
+        person_id: 'person_id',
+        uuid: 'middleName',
+        encounter_id: 'familyName',
+        encounter_datetime: 'familyName2',
+        location_id: '_location_id',
+        location_uuid: '_location_uuid',
+        visit_num: '_visit_num',
+        death_date: '_death_date',
+        scheduled_visit: '_scheduled_visit',
+        transfer_out: '_transfer_out',
+        out_of_care: 'out_of_care',
+        prev_rtc_date: '_prev_rtc_date',
+        rtc_date: '_rtc_date',
+        arv_start_date: '_arv_start_date',
+        arv_first_regimen: '_arv_first_regimen',
+        cur_arv_meds: '_cur_arv_meds',
+        cur_arv_line: '_cur_arv_line',
+        first_evidence_patient_pregnant: '_first_evidence_patient_pregnant',
+        edd: '_edd',
+        screened_for_tb: '_screened_for_tb',
+        tb_tx_start_date: '_tb_tx_start_date',
+        pcp_prophylaxis_start_date: '_pcp_prophylaxis_start_date',
+        cd4_resulted: '_cd4_resulted',
+        cd4_resulted_date: '_cd4_resulted_date',
+        cd4_1: '_cd4_1',
+        cd4_1_date: '_cd4_1_date',
+        cd4_2_date: '_cd4_2_date',
+        cd4_percent_1: '_cd4_percent_1',
+        cd4_percent_1_date: '_cd4_percent_1_date',
+        cd4_percent_2: '_cd4_percent_2',
+        cd4_percent_2_date: '_cd4_percent_2_date',
+        vl_resulted: '_vl_resulted',
+        vl_resulted_date: '_vl_resulted_date',
+        vl_1: '_vl_1',
+        vl_1_date: '_vl_1_date',
+        vl_2: '_vl_2',
+        vl_2_date: '_vl_2_date',
+        vl_order_date: '_vl_order_date',
+        cd4_order_date: '_cd4_order_date',
+        given_name: 'anex',
+        middle_name: 'test',
+        family_name: 'anex',
+        identifiers: '2524040'
+      };
+      /* jshint ignore:end */
+      return appointmentScheduleEtl;
+    }
+
+
 
   }
 })();
---
+++ test/spec/clinic-dashboard/directives/appointment-schedule.directive.test.js
@@ -0,0 +1,121 @@
+/* global chai */
+/* global expect */
+/* global it */
+/* global angular */
+/* global inject */
+/* global beforeEach */
+/* global describe */
+(function () {
+	'use strict';
+
+	describe('Appointment Schedule Directive Unit Tests', function () {
+
+		beforeEach(function () {
+			//debugger;
+			module('ngAmrsApp');
+			module('app.clinicDashboard');
+			module('mock.etlRestServices');
+			module('my.templates');
+		});
+
+		var elm, element, scope, etlRestServiceMock;
+
+		beforeEach(inject(function ($injector, $rootScope, $compile, $httpBackend) {
+			elm = angular.element(
+				'<appointment-schedule location-uuid="{{location.uuid}}" selected="selected">' +
+				'</appointment-schedule>');
+			scope = $rootScope.$new();
+			scope.location = { uuid: 'uuid' };
+			element = $compile(elm)(scope);
+			scope.$digest();
+			etlRestServiceMock = $injector.get('EtlRestService');
+		}));
+
+		it('should have EtlRestServiceMock injected', function () {
+			expect(etlRestServiceMock).to.exist;
+			
+			//ensure that mock services are injected
+			expect(etlRestServiceMock.isMockService).to.equal(true);
+		});
+		
+		it('should call getAppointmentSchedule etl service method when loadSchedule is invoked', function () {
+			var isolateScope = scope.$$childHead;
+			var getAppointmentsSpy = sinon.spy(etlRestServiceMock, 'getAppointmentSchedule');
+			
+			console.log("number of calls:" + getAppointmentsSpy.callCount)
+			isolateScope.loadSchedule();
+			
+			chai.expect(getAppointmentsSpy.callCount).to.equal(1);
+			
+		});
+		
+		it('should call loadSchedule method when selectedDate() is invoked with a date', function () {
+			
+			//we know that loadShedule in turns calls getAppointments when all conditions met
+			//we therefore ensure that all conditions are met for loadSchedule to trigger getAppointments
+			
+			var isolateScope = scope.$$childHead; //get child scope. there's only one in this case
+			
+			var getAppointmentsSpy = sinon.spy(etlRestServiceMock, 'getAppointmentSchedule');
+			
+			console.log("number of calls:" + getAppointmentsSpy.callCount)
+			
+			isolateScope.selectedDate('2015-08-09');
+			
+			chai.expect(getAppointmentsSpy.callCount).to.equal(1);
+			
+		});
+		
+		it('should call loadSchedule method when location uuid changes', function () {
+			
+			var getAppointmentsSpy = sinon.spy(etlRestServiceMock, 'getAppointmentSchedule');
+			
+			scope.location.uuid = 'uuid2';
+			
+			scope.$digest();
+			
+			chai.expect(getAppointmentsSpy.callCount).to.equal(1);
+			
+		});
+		
+		it('should call loadSchedule on loading of the controller, and attach appointments to the scope', function () {
+			//at this point, the location uuid changed on load and therefore appointments are present
+			//by default the etservicemock will return 20 appointments
+			var isolateScope = scope.$$childHead; 
+			
+			chai.expect(isolateScope.patients).to.not.equal(10);
+			
+		});
+		
+		it('should display error message when loading of appointments fails', function () {
+			var isolateScope = scope.$$childHead; 
+			
+			console.log("Has Loading Errors:" + isolateScope.experiencedLoadingError); 
+			
+			etlRestServiceMock.returnErrorOnNextCall = true;
+			
+			isolateScope.loadSchedule();
+			
+			console.log("Has Loading Errors:" + isolateScope.experiencedLoadingError);
+			
+			chai.expect(isolateScope.experiencedLoadingError).to.equal(true);
+			
+		});
+		
+		it('should set the selected date to the argument when viewDayAppointments broadcast is received', function () {
+			var isolateScope = scope.$$childHead; 
+			
+			scope.$broadcast('viewDayAppointments', '2015-08-08');
+			
+			scope.$digest();
+			
+			chai.expect(isolateScope.selectedDate()).to.equal( '2015-08-08');
+			
+		});
+		
+		
+
+
+
+	});
+})();
