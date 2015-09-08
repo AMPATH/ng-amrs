1457f92561f49dbbfbbe7c701251a97a2cb931fb
nkimaina <nkimaina@ampath.or.ke>
2015-09-08 11:21:27 +0300

    AA-129: Added functionality to persist bas url for openmrs settings service and unit tests

--- app/scripts/openmrs-rest-services/openmrs-settings.service.js
+++ app/scripts/openmrs-rest-services/openmrs-settings.service.js
@@ -1,32 +1,62 @@
 /*jshint -W003, -W026, -W117, -W098 */
-(function() {
+(function () {
   'use strict';
 
   angular
-        .module('app.openmrsRestServices')
-        .service('OpenmrsSettings', OpenmrsSettings);
+    .module('app.openmrsRestServices')
+    .service('OpenmrsSettings', OpenmrsSettings);
 
-  OpenmrsSettings.$inject = [];
+  OpenmrsSettings.$inject = ['$cookieStore'];
 
-  function OpenmrsSettings() {
+  function OpenmrsSettings($cookieStore) {
     var serviceDefinition;
     var restUrlBaseList = ['https://test1.ampath.or.ke:8443/amrs/ws/rest/v1/', 'https://etl1.ampath.or.ke:8443/amrs/ws/rest/v1/', 'http://localhost:8080/openmrs/ws/rest/v1/'];
     var restUrlBase = restUrlBaseList[0];
-
+    
+    initialize();
     serviceDefinition = {
-          getCurrentRestUrlBase: getCurrentRestUrlBase,
-          setCurrentRestUrlBase: setCurrentRestUrlBase,
-          restUrlBase: restUrlBase
-        };
+      reInitialize: initialize,
+      getCurrentRestUrlBase: getCurrentRestUrlBase,
+      setCurrentRestUrlBase: setCurrentRestUrlBase,
+      restUrlBase: restUrlBase,
+      addUrlToList: addUrlToList,
+      getUrlBaseList: getUrlBaseList,
+      hasCoockiePersistedCurrentUrlBase: hasCoockiePersistedCurrentUrlBase
+    };
     return serviceDefinition;
+    
+    function initialize(){
+      
+      var lastSetUrl = $cookieStore.get('restUrlBase');
+      
+      if(lastSetUrl) 
+        restUrlBase = lastSetUrl;
+    }
+    
+    function hasCoockiePersistedCurrentUrlBase(){
+      var lastSetUrl = $cookieStore.get('restUrlBase');
+      
+      if(lastSetUrl)
+        return true;
+        
+      return false;
+    }
 
     function getCurrentRestUrlBase() {
-          return restUrlBase;
-        }
+      return restUrlBase;
+    }
 
     function setCurrentRestUrlBase(url) {
-          restUrlBase = url;
-        }
+      restUrlBase = url;
+      $cookieStore.put('restUrlBase',url);
+    }
 
+    function getUrlBaseList() {
+      return restUrlBaseList;
+    }
+
+    function addUrlToList(url) {
+      restUrlBaseList.push(url);
+    }
   }
 })();
