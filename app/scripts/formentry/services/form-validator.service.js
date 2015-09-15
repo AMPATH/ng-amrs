f1caa73acf3eadbdcfad101f3989a464b62d6274
nkimaina <nkimaina@ampath.or.ke>
2015-09-15 16:05:21 +0300

    AA-122: Merged adult return form changes

--- app/scripts/formentry/services/form-validator.service.js
+++ app/scripts/formentry/services/form-validator.service.js
@@ -137,7 +137,8 @@ jshint -W106, -W052, -W098, -W003, -W068, -W004, -W033, -W030, -W117, -W116, -W0
         }
 
         function isEmpty(val) {
-            if (val === undefined || val === null || val === ''|| val === 'null') {
+
+            if (val === undefined || val === null || val === '' || val === 'null') {
                 return true;
             }
             return false;
