851dbe4d93d58a3e49ec591b6b340685f261c3e0
kimaina <kimkenal@gmail.com>
2015-09-25 02:43:52 -0400

    AA-201: Appointment-schedule fixing search/filtering of patient

--- app/scripts/clinic-dashboard/directives/appointment-schedule.directive.js
+++ app/scripts/clinic-dashboard/directives/appointment-schedule.directive.js
@@ -30,6 +30,7 @@ jshint -W003, -W026
         $scope.isBusy = false;
         $scope.experiencedLoadingError = false;
         $scope.currentPage = 1;
+
         $scope.loadSchedule = loadSchedule;
         $scope.loadPatient = loadPatient;
         $scope.$on('viewDayAppointments',onViewDayAppointmentBroadcast);
