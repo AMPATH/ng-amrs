57b3dea5813db9ba7fdc91507893766e2556f86b
kimaina <kimkenal@gmail.com>
2015-10-01 09:25:45 -0400

    AA-215 Change health center Id to Name

--- app/scripts/openmrs-rest-services/openmrs-patient.service.js
+++ app/scripts/openmrs-rest-services/openmrs-patient.service.js
@@ -18,8 +18,7 @@
     return service;
     function getResource() {
           var v = 'custom:(uuid,identifiers:(identifier,uuid,identifierType:(uuid,name)),person:(uuid,gender,birthdate,dead,age,deathDate,preferredName:(givenName,middleName,familyName),';
-          v = v  + 'attributes:(uuid,value,attributeType:ref),preferredAddress:(preferred,address1,address2,cityVillage,stateProvince,country,postalCode,countyDistrict,address3,address4,address5,address6)))';
-
+          v = v  + 'attributes,preferredAddress:(preferred,address1,address2,cityVillage,stateProvince,country,postalCode,countyDistrict,address3,address4,address5,address6)))';
       var r = $resource(OpenmrsSettings.getCurrentRestUrlBase().trim() + 'patient/:uuid',
                 {uuid: '@uuid', v: v},
                 {query: {method: 'GET', isArray: false}});
