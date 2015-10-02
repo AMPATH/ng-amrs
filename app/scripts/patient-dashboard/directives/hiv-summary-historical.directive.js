a127321a518f0e3217ced6bd2c9791fe8ba7dea7
kimaina <kimkenal@gmail.com>
2015-10-02 08:33:52 -0400

    AA-216: remove parentheses if value is empty In HIV Summary

--- app/scripts/patient-dashboard/directives/hiv-summary-historical.directive.js
+++ app/scripts/patient-dashboard/directives/hiv-summary-historical.directive.js
@@ -20,80 +20,77 @@ jshint -W003, -W026
     }
 
     hivSummaryHistoricalController.$inject = ['$scope', 'EtlRestService', 'HivSummaryModel'];
-
     function hivSummaryHistoricalController($scope, EtlRestService, HivSummaryModel) {
-		//non-function types scope members
+		    //non-function types scope members
         $scope.hivSummaries = [];
-        
         $scope.isBusy = false;
         $scope.experiencedLoadingErrors = false;
-        
-        //paging variables 
+
+        //paging variables
         $scope.nextStartIndex = 0;
         $scope.pageSize = 10;
         $scope.allDataLoaded = false;
-        
+        $scope.summaryLength;
+
         //function types scope members
         $scope.loadHistoricalHivSummary = loadHistoricalHivSummary;
-        
+
         activate();
-        
+
         function activate() {
             $scope.$on('viewHivHistoricalSummary',viewHivHistoricalSummary);
         }
-        
-        
+
         function viewHivHistoricalSummary() {
             loadHistoricalHivSummary();
         }
-        
+
         function loadHistoricalHivSummary() {
              $scope.experiencedLoadingErrors = false;
-             
+
              if($scope.isBusy === true) return;
-             
+
              $scope.isBusy = true;
-            
+
              if ($scope.patientUuid && $scope.patientUuid !== '')
-                EtlRestService.getHivSummary($scope.patientUuid, $scope.nextStartIndex, $scope.pageSize, onFetchHivSummarySuccess, onFetchHivSummaryFailed);
+                EtlRestService.getHivSummary($scope.patientUuid, $scope.nextStartIndex, $scope.pageSize,
+                  onFetchHivSummarySuccess, onFetchHivSummaryFailed);
         }
-        
-        
+
         function onFetchHivSummarySuccess(records){
             $scope.isBusy = false;
+            $scope.summaryLength=0;
             for (var e in records.result) {
+              var testData =records.result[e];
+              if(testData.cd4_1_date||testData.vl_1_date)
+              {
+                $scope.summaryLength= $scope.summaryLength+1;
                 $scope.hivSummaries.push(new HivSummaryModel.hivSummary(records.result[e]));
+              }
             }
-            
             $scope.nextStartIndex = +records.startIndex + records.size;
-            
-            if (records.size === 0) {
-               $scope.allDataLoaded = true;
+          if (records.size !== 0) {
+            if ($scope.summaryLength===0)
+            {
+              loadHistoricalHivSummary();
             }
+            else {
+              $scope.isBusy = false;
+            }
+          }
+          else {
+            $scope.allDataLoaded = true;
+          }
         }
-        
+
         function onFetchHivSummaryFailed(error) {
             $scope.isBusy = false;
-            $scope.experiencedLoadingErrors = true;
-            
+          $scope.experiencedLoadingErrors = true;
         }
-        
 	}
-	
+
 	function hivSummaryHistoricalLink(scope, element, attrs, vm) {
-        // attrs.$observe('patientUuid', onPatientUuidChanged);
 
-        // function onPatientUuidChanged(newVal, oldVal) {
-        //     if (newVal && newVal != "") {
-        //         scope.isBusy = false;
-        //         scope.allDataLoaded = false;
-        //         scope.nextStartIndex = 0;
-        //         scope.encounters = [];
-        //         scope.loadMoreLabs();
-        //     }
-        // }
     }
-
 })();
-	
-	
+
