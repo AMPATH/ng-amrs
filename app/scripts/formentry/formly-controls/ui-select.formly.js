bb54afeace542a553bfb2225e8ffc115ad230ed2
nkimaina <nkimaina@ampath.or.ke>
2015-10-06 16:15:14 +0300

    AA-175: Flagging of custom formly types when invalid done.

--- app/scripts/formentry/formly-controls/ui-select.formly.js
+++ app/scripts/formentry/formly-controls/ui-select.formly.js
@@ -12,7 +12,7 @@ jshint -W106, -W098, -W109, -W003, -W068, -W004, -W033, -W030, -W117, -W116, -W0
     mod.run(function (formlyConfig) {
         formlyConfig.setType({
             name: 'ui-select-extended',
-            wrapper: ['bootstrapLabel','validation'],
+            wrapper: ['bootstrapLabel','bootstrapHasError','validation'],
             template: '<ui-select ng-model="model[options.key]" theme="bootstrap" ng-required="{{to.required}}" ng-disabled="{{to.disabled}}" reset-search-input="false"> <ui-select-match placeholder="{{to.placeholder}}"> {{evaluateFunction($select.selected[to.labelProp || \'name\'])}} </ui-select-match> <ui-select-choices refresh="refreshItemSource($select.search)" group-by="to.groupBy" repeat="(evaluateFunction(option[to.valueProp || \'value\'])) as option in itemSource" > <div ng-bind-html="evaluateFunction(option[to.labelProp || \'name\']) | highlight: $select.search"></div> </ui-select-choices> </ui-select>',
             link: function (scope, el, attrs, vm) {
                 //incase we need link function
