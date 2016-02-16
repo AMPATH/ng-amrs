6321dba7a2291c380f905ffaac97730d09954fc0
Nicky Kibor <nickykimaina@gmail.com>
2016-02-16 16:05:40 +0300

    AA-510: Fixed submit and navigation bug resulting from integration

--- app/scripts/formentry/formentry.module.js
+++ app/scripts/formentry/formentry.module.js
@@ -1,15 +1,15 @@
 /*jscs:disable disallowMixedSpacesAndTabs, requireDotNotation, requirePaddingNewLinesBeforeLineComments, requireTrailingComma*/
-(function() {
-  'use strict';
+(function () {
+    'use strict';
 
-  angular
+    angular
         .module('app.formentry', [
             'formlyBootstrap',
             'app.openmrsRestServices',
             'ngMessages',
             'ui.bootstrap',
             'ui.bootstrap.datetimepicker',
-	        'ui.select',
+            'ui.select',
             'ui.router',
             'angularMoment',
             'dialogs.main',
@@ -20,57 +20,65 @@
             'sticky',
             'openmrs.angularFormentry'
         ])
-    .config(function($stateProvider) {
-      $stateProvider
-          .state('forms', {
-            url: '/form/:formuuid/patient/:uuid',
-            templateUrl: 'views/formentry/formentry.html',
-            controller: 'FormentryCtrl',
-            controllerAs: 'vm',
-            data: { requireLogin: true }
-          })
-        .state('visit', {
-          url: '/form/:formuuid/patient/:uuid/visit/:visitUuid',
-          templateUrl: 'views/formentry/formentry.html',
-          controller: 'FormentryCtrl',
-          controllerAs: 'vm',
-          data: { requireLogin: true }
+        .config(function ($stateProvider) {
+            $stateProvider
+                .state('forms', {
+                    url: '/form/:formuuid/patient/:uuid',
+                    templateUrl: 'views/formentry/formentry.html',
+                    controller: 'FormentryCtrl',
+                    controllerAs: 'vm',
+                    data: { requireLogin: true }
+                })
+                .state('visit', {
+                    url: '/form/:formuuid/patient/:uuid/visit/:visitUuid',
+                    templateUrl: 'views/formentry/formentry.html',
+                    controller: 'FormentryCtrl',
+                    controllerAs: 'vm',
+                    data: { requireLogin: true }
+                })
+                .state('tabs', {
+                    url: '/form/tabs',
+                    templateUrl: 'views/formentry/tab.html',
+                    controller: 'tabCtrl',
+                    controllerAs: 'vm',
+                    data: { requireLogin: true }
+                });
         })
-        .state('tabs', {
-          url: '/form/tabs',
-          templateUrl: 'views/formentry/tab.html',
-          controller: 'tabCtrl',
-          controllerAs: 'vm',
-          data: { requireLogin: true }
-        });
-    })
-    .run(['FormentryConfig','OpenmrsSettings', function(FormentryConfig, OpenmrsSettings){
-        FormentryConfig.setOpenmrsBaseUrl(OpenmrsSettings.getCurrentRestUrlBase());
-    }])
-  .config(['dialogsProvider','$translateProvider', function(dialogsProvider,$translateProvider) {
-    dialogsProvider.useBackdrop('static');
-		dialogsProvider.useEscClose(false);
-		dialogsProvider.useCopy(false);
-		dialogsProvider.setSize('sm');
+        .run(['FormentryConfig', 'OpenmrsSettings', function (FormentryConfig, OpenmrsSettings) {
+            FormentryConfig.setOpenmrsBaseUrl(OpenmrsSettings.getCurrentRestUrlBase());
+        }])
+        .run(function (formlyConfig, formlyValidationMessages, formlyApiCheck) {
+            formlyConfig.extras.errorExistsAndShouldBeVisibleExpression = 'fc.$touched || form.$submitted';
+            formlyValidationMessages.addStringMessage('required', 'Ops! This field is required');
+            formlyValidationMessages.addTemplateOptionValueMessage('max', 'max', 'The max value allowed is ', '', 'Too Big');
+            formlyValidationMessages.addTemplateOptionValueMessage('min', 'min', 'The min value allowed is ', '', 'Too Small');
+            formlyConfig.disableWarnings = true;
+            //formlyApiCheck.globalConfig.disabled = true;
+        })
+        .config(['dialogsProvider', '$translateProvider', function (dialogsProvider, $translateProvider) {
+            dialogsProvider.useBackdrop('static');
+            dialogsProvider.useEscClose(false);
+            dialogsProvider.useCopy(false);
+            dialogsProvider.setSize('sm');
 
-		$translateProvider.translations('es', {
-      DIALOGS_ERROR: 'Error',
-			DIALOGS_ERROR_MSG: 'Se ha producido un error desconocido.',
-			DIALOGS_CLOSE: 'Cerca',
-			DIALOGS_PLEASE_WAIT: 'Espere por favor',
-			DIALOGS_PLEASE_WAIT_ELIPS:'Espere por favor...',
-			DIALOGS_PLEASE_WAIT_MSG: 'Esperando en la operacion para completar.',
-			DIALOGS_PERCENT_COMPLETE: '% Completado',
-			DIALOGS_NOTIFICATION: 'Notificacion',
-			DIALOGS_NOTIFICATION_MSG: 'Notificacion de aplicacion Desconocido.',
-			DIALOGS_CONFIRMATION: 'Confirmacion',
-			DIALOGS_CONFIRMATION_MSG: 'Se requiere confirmacion',
-			DIALOGS_OK: 'Bueno',
-			DIALOGS_YES: 'Si',
-			DIALOGS_NO: 'No'
-		});
+            $translateProvider.translations('es', {
+                DIALOGS_ERROR: 'Error',
+                DIALOGS_ERROR_MSG: 'Se ha producido un error desconocido.',
+                DIALOGS_CLOSE: 'Cerca',
+                DIALOGS_PLEASE_WAIT: 'Espere por favor',
+                DIALOGS_PLEASE_WAIT_ELIPS: 'Espere por favor...',
+                DIALOGS_PLEASE_WAIT_MSG: 'Esperando en la operacion para completar.',
+                DIALOGS_PERCENT_COMPLETE: '% Completado',
+                DIALOGS_NOTIFICATION: 'Notificacion',
+                DIALOGS_NOTIFICATION_MSG: 'Notificacion de aplicacion Desconocido.',
+                DIALOGS_CONFIRMATION: 'Confirmacion',
+                DIALOGS_CONFIRMATION_MSG: 'Se requiere confirmacion',
+                DIALOGS_OK: 'Bueno',
+                DIALOGS_YES: 'Si',
+                DIALOGS_NO: 'No'
+            });
 
-		$translateProvider.preferredLanguage('en-US');
-	}]);
+            $translateProvider.preferredLanguage('en-US');
+        }]);
 
 })();
--- app/scripts/openmrs-rest-services/encounter.service.js
+++ app/scripts/openmrs-rest-services/encounter.service.js
@@ -1,145 +1,176 @@
 /*
 jshint -W026, -W116, -W098, -W003, -W068, -W069, -W004, -W033, -W030, -W117
 */
-(function() {
-  'use strict';
+(function () {
+    'use strict';
 
-  angular
-    .module('app.openmrsRestServices')
-          .factory('EncounterResService', EncounterResService);
+    angular
+        .module('app.openmrsRestServices')
+        .factory('EncounterResService', EncounterResService);
 
-  EncounterResService.$inject = ['Restangular'];
+    EncounterResService.$inject = ['Restangular', 'OpenmrsSettings', '$resource'];
 
-  function EncounterResService(Restangular) {
-    var service = {
-      getEncounterByUuid: getEncounterByUuid,
-      saveEncounter: saveEncounter,
-      getPatientEncounters: getPatientEncounters,
-      voidEncounter: voidEncounter
-    }
+    function EncounterResService(Restangular, OpenmrsSettings, $resource) {
+        var service = {
+            getEncounterByUuid: getEncounterByUuid,
+            saveEncounter: saveEncounter,
+            getPatientEncounters: getPatientEncounters,
+            voidEncounter: voidEncounter
+        };
 
-    return service;
+        return service;
 
-    function voidEncounter(uuid, successCallback, errorCallback) {
-        Restangular.one('encounter', uuid).remove().then(function(response) {
-            if(typeof successCallback === 'function') {
-                successCallback(response);
-            }
-        }, function(error) {
-            if(typeof errorCallback === 'function') {
-                errorCallback(error);
-            }
-        });
-    }
-
-    function getEncounterByUuid(params, successCallback, errorCallback) {
-      var objParams = {};
-      var _customDefaultRep = 'custom:(uuid,encounterDatetime,' +
-                      'patient:(uuid,uuid),form:(uuid,name),' +
-                      'location:ref,encounterType:ref,provider:ref,' +
-                      'obs:(uuid,obsDatetime,concept:(uuid,uuid),value:ref,groupMembers))';
-
-      if(angular.isDefined(params) && typeof params === 'string'){
-        var encounterUuid = params;
-        objParams = {'encounter': encounterUuid, 'v': _customDefaultRep}
-      } else {
-        objParams = {
-          'encounter': params.uuid,
-          'v': params.rep || _customDefaultRep
-        }
-      }
-      Restangular.one('encounter',objParams.encounter).get({v:objParams.v}).then(function(data) {
-        _successCallbackHandler(successCallback, data);
-      },
-      function(error) {
-        console.log('An error occured while attempting to fetch encounter ' +
-                    'with uuid ' + params.patientUuid);
-        if (typeof errorCallback === 'function') errorCallback(error);
-      });
-    }
-
-    function saveEncounter(encounter, successCallback, errorCallback) {
-
-      var _encounter =JSON.parse(encounter);
-      if(_encounter.uuid !== undefined)
-      {
-        var uuid = _encounter.uuid
-        delete _encounter['uuid'];
-        //Restangular.one('encounter',uuid).remove(); // void encounter
-
-        //_encounter['voided'] = false;
-        console.log('update json');
-        console.log(JSON.stringify(_encounter));
-        //updating an existing encounter
-        Restangular.one('encounter', uuid).customPOST(JSON.stringify(_encounter)).then(function(success) {
-          console.log('Encounter saved successfully');
-          if (typeof successCallback === 'function') successCallback(success);
-        },
-        function(error) {
-          console.log('Error saving encounter');
-          if (typeof errorCallback === 'function') errorCallback(error);
-        });
-      }
-      else {
-        Restangular.service('encounter').post(encounter).then(function(success) {
-          console.log('Encounter saved successfully');
-          if (typeof successCallback === 'function') successCallback(success);
-        },
-        function(error) {
-          console.log('Error saving encounter');
-          if (typeof errorCallback === 'function') errorCallback(error);
-        });
-      }
-    }
-
-    function getPatientEncounters(params, successCallback, errorCallback) {
-      var objParams = {};
-
-      // Don't include obs by default
-      var _customDefaultRep = 'custom:(uuid,encounterDatetime,' +
-                      'patient:(uuid,uuid),form:(uuid,name),' +
-                      'location:ref,encounterType:ref,provider:ref)';
-
-      if(angular.isDefined(params) && typeof params === 'string'){
-        var patientUuid = params;
-        objParams = {'patient': patientUuid, 'v':_customDefaultRep}
-      } else {
-        var v = params.rep || params.v;
-        objParams = {
-          'patient': params.patientUuid,
-          'v': v || _customDefaultRep
+        function getResource() {
+            var v = 'custom:(uuid,encounterDatetime,' +
+                'patient:(uuid,uuid),form:(uuid,name),' +
+                'location:ref,encounterType:ref,provider:ref,' +
+                'obs:(uuid,obsDatetime,concept:(uuid,uuid),value:ref,groupMembers))';
+            return $resource(OpenmrsSettings.getCurrentRestUrlBase().trim() + 'encounter/:uuid',
+                { uuid: '@uuid', v: v },
+                { query: { method: 'GET', isArray: false } });
         }
 
-        /* jshint ignore: start */
-        delete params.patientUuid;
-        delete params.rep;
-        /* jshint ignore: end */
+        function voidEncounter(uuid, successCallback, errorCallback) {
+            Restangular.one('encounter', uuid).remove().then(function (response) {
+                if (typeof successCallback === 'function') {
+                    successCallback(response);
+                }
+            }, function (error) {
+                if (typeof errorCallback === 'function') {
+                    errorCallback(error);
+                }
+            });
+        }
 
-        //Add objParams to params and assign it back objParams
-        params.patient = objParams.patient;
-        params.v = objParams.v;
+        function getEncounterByUuid(params, successCallback, errorCallback) {
+            var objParams = {};
+            var _customDefaultRep = 'custom:(uuid,encounterDatetime,' +
+                'patient:(uuid,uuid),form:(uuid,name),' +
+                'location:ref,encounterType:ref,provider:ref,' +
+                'obs:(uuid,obsDatetime,concept:(uuid,uuid),value:ref,groupMembers))';
 
-        objParams = params;
-      }
+            if (angular.isDefined(params) && typeof params === 'string') {
+                var encounterUuid = params;
+                objParams = { 'encounter': encounterUuid, 'v': _customDefaultRep };
+            } else {
+                objParams = {
+                    'encounter': params.uuid,
+                    'v': params.rep || _customDefaultRep
+                };
+            }
+            Restangular.one('encounter', objParams.encounter).get({ v: objParams.v }).then(function (data) {
+                _successCallbackHandler(successCallback, data);
+            },
+                function (error) {
+                    console.log('An error occured while attempting to fetch encounter ' +
+                        'with uuid ' + params.patientUuid);
+                    if (typeof errorCallback === 'function') errorCallback(error);
+                });
+        }
 
-      Restangular.one('encounter').get(objParams).then(function(data) {
-        if(angular.isDefined(data.results)) data = data.results;
-        _successCallbackHandler(successCallback, data.reverse());
-      },
-      function(error) {
-        console.log('An error occured while attempting to fetch encounters ' +
-                    'for patient with uuid ' + params.patientUuid);
-        if (typeof errorCallback === 'function') errorCallback(error);
-      });
+        function saveEncounter(encounter, successCallback, errorCallback) {
+
+            var _encounter = JSON.parse(encounter);
+            var encounterResource = getResource();
+
+            if (_encounter.uuid !== undefined) {
+                var uuid = _encounter.uuid;
+                delete _encounter['uuid'];
+
+                console.log('update json');
+                console.log(JSON.stringify(_encounter));
+                //updating an existing encounter
+                // Restangular.one('encounter', uuid).customPOST(JSON.stringify(_encounter)).then(function (success) {
+                //     console.log('Encounter saved successfully');
+                //     if (typeof successCallback === 'function') successCallback(success);
+                // },
+                //     function (error) {
+                //         console.log('Error saving encounter');
+                //         if (typeof errorCallback === 'function') errorCallback(error);
+                //    });
+
+                encounterResource.save({ uuid: uuid }, JSON.stringify(_encounter)).$promise
+                    .then(function (data) {
+                        console.log('Encounter saved successfully');
+                        if (typeof successCallback === 'function') successCallback(data);
+                    })
+                    .catch(function (error) {
+                        console.log('Error saving encounter');
+                        if (typeof errorCallback === 'function')
+                            errorCallback(error);
+                    });
+            }
+            else {
+                // Restangular.service('encounter').post(encounter).then(function (success) {
+                //     console.log('Encounter saved successfully');
+                //     if (typeof successCallback === 'function') successCallback(success);
+                // },
+                //     function (error) {
+                //         console.log('Error saving encounter');
+                //         if (typeof errorCallback === 'function') errorCallback(error);
+                //     });
+                encounterResource.save(encounter).$promise
+                    .then(function (data) {
+                        console.log('Encounter saved successfully');
+                        if (typeof successCallback === 'function')
+                            successCallback(data);
+                    })
+                    .catch(function (error) {
+                        console.log('Error saving encounter');
+                        if (typeof errorCallback === 'function')
+                            errorCallback(error);
+                    });
+            }
+        }
+
+        function getPatientEncounters(params, successCallback, errorCallback) {
+            var objParams = {};
+
+            // Don't include obs by default
+            var _customDefaultRep = 'custom:(uuid,encounterDatetime,' +
+                'patient:(uuid,uuid),form:(uuid,name),' +
+                'location:ref,encounterType:ref,provider:ref)';
+
+            if (angular.isDefined(params) && typeof params === 'string') {
+                var patientUuid = params;
+                objParams = { 'patient': patientUuid, 'v': _customDefaultRep }
+            } else {
+                var v = params.rep || params.v;
+                objParams = {
+                    'patient': params.patientUuid,
+                    'v': v || _customDefaultRep
+                };
+
+                /* jshint ignore: start */
+                delete params.patientUuid;
+                delete params.rep;
+                /* jshint ignore: end */
+
+                //Add objParams to params and assign it back objParams
+                params.patient = objParams.patient;
+                params.v = objParams.v;
+
+                objParams = params;
+            }
+
+            Restangular.one('encounter').get(objParams).then(function (data) {
+                if (angular.isDefined(data.results)) data = data.results;
+                _successCallbackHandler(successCallback, data.reverse());
+            },
+                function (error) {
+                    console.log('An error occured while attempting to fetch encounters ' +
+                        'for patient with uuid ' + params.patientUuid);
+                    if (typeof errorCallback === 'function') errorCallback(error);
+                });
+        }
+
+        function _successCallbackHandler(successCallback, data) {
+            if (typeof successCallback !== 'function') {
+                console.log('Error: You need a callback function to process' +
+                    ' results');
+                return;
+            }
+            successCallback(data);
+        }
     }
-
-    function _successCallbackHandler(successCallback, data) {
-      if (typeof successCallback !== 'function') {
-        console.log('Error: You need a callback function to process' +
-        ' results');
-        return;
-      }
-      successCallback(data);
-    }
-  }
 })();
