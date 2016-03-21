e39138f2a4e4e7c6109bd9d87f76cc4f31a5286f
Nicky Kibor <nickykimaina@gmail.com>
2016-03-21 11:41:53 +0300

    AA-571: Fixed todays vitals bug not displaying

--- app/scripts/models/today-vital.model.js
+++ app/scripts/models/today-vital.model.js
@@ -19,26 +19,39 @@
             var modelDefinition = {};
             for (var i = 0; i < obsArray.length; i++) {
                 var ob = obsArray[i];
-                if (ob.concept.uuid === 'a8a65d5a-1350-11df-a1f1-0026b9348838') {
-                    modelDefinition.systolic = ob.value;
-                } else if (ob.concept.uuid === 'a8a65e36-1350-11df-a1f1-0026b9348838') {
-                    modelDefinition.diastolic = ob.value;
-                } else if (ob.concept.uuid === 'a8a65f12-1350-11df-a1f1-0026b9348838') {
-                    modelDefinition.pulse = ob.value;
-                } else if (ob.concept.uuid === 'a8a65fee-1350-11df-a1f1-0026b9348838') {
-                    modelDefinition.temperature = ob.value;
-                } else if (ob.concept.uuid === 'a8a66354-1350-11df-a1f1-0026b9348838') {
-                    modelDefinition.oxygenSaturation = ob.value;
-                } else if (ob.concept.uuid === 'a8a6619c-1350-11df-a1f1-0026b9348838') {
-                    modelDefinition.height = ob.value;
-                } else if (ob.concept.uuid === 'a8a660ca-1350-11df-a1f1-0026b9348838') {
-                    modelDefinition.weight = ob.value;
+
+                if (ob.concept.uuid === 'a899e6d8-1350-11df-a1f1-0026b9348838') {
+                    ob = ob.groupMembers;
+                    for(var j = 0; j < ob.length; j++ ){
+                         populateModel(modelDefinition, ob[j]);
+                    }
+                } else {
+                    populateModel(modelDefinition, ob);
                 }
+
             }
-            
-            modelDefinition.bmi = calcBMI(modelDefinition.height,  modelDefinition.weight);
+
+            modelDefinition.bmi = calcBMI(modelDefinition.height, modelDefinition.weight);
 
             return modelDefinition;
+        }
+
+        function populateModel(modelDefinition, ob) {
+            if (ob.concept.uuid === 'a8a65d5a-1350-11df-a1f1-0026b9348838') {
+                modelDefinition.systolic = ob.value;
+            } else if (ob.concept.uuid === 'a8a65e36-1350-11df-a1f1-0026b9348838') {
+                modelDefinition.diastolic = ob.value;
+            } else if (ob.concept.uuid === 'a8a65f12-1350-11df-a1f1-0026b9348838') {
+                modelDefinition.pulse = ob.value;
+            } else if (ob.concept.uuid === 'a8a65fee-1350-11df-a1f1-0026b9348838') {
+                modelDefinition.temperature = ob.value;
+            } else if (ob.concept.uuid === 'a8a66354-1350-11df-a1f1-0026b9348838') {
+                modelDefinition.oxygenSaturation = ob.value;
+            } else if (ob.concept.uuid === 'a8a6619c-1350-11df-a1f1-0026b9348838') {
+                modelDefinition.height = ob.value;
+            } else if (ob.concept.uuid === 'a8a660ca-1350-11df-a1f1-0026b9348838') {
+                modelDefinition.weight = ob.value;
+            }
         }
 
         function calcBMI(height, weight) {
