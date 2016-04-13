5ea3f931e2a089ac7e401db5a86afe2137ab4b9c
makombe <kennedymakombe@gmail.com>
2016-04-13 13:40:03 +0300

    AA-596: Added test ordered column to lab data summary

--- app/scripts/patient-dashboard/directives/labs-summary.directive.js
+++ app/scripts/patient-dashboard/directives/labs-summary.directive.js
@@ -19,8 +19,8 @@ jshint -W003, -W026
         };
     }
 
-    labsSummaryController.$inject = ['$scope', 'EtlRestService', 'PatientTestModel', 'UtilService'];
-    function labsSummaryController($scope, EtlRestService, patientTestModel, UtilService) {
+    labsSummaryController.$inject = ['$scope', 'EtlRestService', 'PatientTestModel', 'UtilService','$filter'];
+    function labsSummaryController($scope, EtlRestService, patientTestModel, UtilService,$filter) {
         $scope.injectedEtlRestService = EtlRestService;
         $scope.encounters = [];
         $scope.isBusy = false;
@@ -28,51 +28,211 @@ jshint -W003, -W026
         $scope.loadMoreLabs = loadMoreLabs;
         $scope.allDataLoaded = false;
         $scope.experiencedLoadingError = false;
-        $scope.testLength;
+        $scope.fixedColumns=true;
+       // $scope.testLength;
 
-        function loadMoreLabs() {
+      $scope.labTestTags =[
+        {
+          name: 'test_datetime',
+          headers:'date '
+        },
+        {
+          name: 'cd4_count',
+          headers:'cd4_count'
+        },
+        {
+          name: 'cd4_percent',
+          headers:'cd4_percent'
+        },
+        {
+          name: 'hiv_viral_load',
+          headers:'hiv_viral_load'
+        },
+        {
+          name: 'hemoglobin',
+          headers:'hemoglobin'
+        },
+        {
+          name: 'ast',
+          headers:'ast'
+        },
+        {
+          name: 'creatinine',
+          headers:'creatinine'
+        },
+        {
+          name: 'chest_xray',
+          headers:'chest_xray'
+        },
+        {
+          name: 'tests_ordered',
+          headers:'tests_ordered'
+        }
+      ] ;
+      //DataTable Options
+      $scope.columns = [];
+      $scope.bsTableControl = {options: {}};
+      $scope.exportList = [
+        {name: 'Export Basic', value: ''},
+        {name: 'Export All', value: 'all'},
+        {name: 'Export Selected', value: 'selected'}];
+      $scope.exportDataType = $scope.exportList[1];
+      $scope.updateSelectedType = function () {
+        console.log($scope.exportDataType.value, $scope.exportDataType.name);
+        var bsTable = document.getElementById('bsTable');
+        var element = angular.element(bsTable);
+        element.bootstrapTable('refreshOptions', {
+          exportDataType: $scope.exportDataType.value
+        });
+      };
+
+      function loadMoreLabs(loadNextOffset) {
+        $scope.experiencedLoadingErrors = false;
             if ($scope.isBusy === true) return;
-
-            $scope.isBusy = true;
-            $scope.experiencedLoadingError = false;
+          //  $scope.experiencedLoadingError = false;
+        if(loadNextOffset!==true)resetPaging();
+         $scope.isBusy = true;
 
             if ($scope.patientUuid && $scope.patientUuid !== '')
-                  EtlRestService.getPatientTests($scope.patientUuid, $scope.nextStartIndex, 10,
+                  EtlRestService.getPatientTests($scope.patientUuid, $scope.nextStartIndex, 20,
                     onFetchPatientTestsSuccess, onFetchPatientTestsFailed);
         }
 
+      function resetPaging(){
+        $scope.nextStartInts = [];
+        $scope.allDataLoaded = false;
+        $scope.nextStartIndex = 0;
+        $scope.encounters = [];
+      }
         function onFetchPatientTestsSuccess(patientTestsData) {
-            $scope.nextStartIndex = +patientTestsData.startIndex + patientTestsData.size;
-            $scope.testLength=0;
-            for (var e in patientTestsData.result) {
-                  var testData =patientTestsData.result[e];
-                  var membersToCheck = ['cd4_count','cd4_percent','hiv_viral_load','hemoglobin',
-                  'ast','creatinine','chest_xray'];
-                  if(!UtilService.hasAllMembersUndefinedOrNull(testData, membersToCheck)) {
-                    $scope.testLength= $scope.testLength+1;
-                    $scope.encounters.push(new patientTestModel.patientTest(patientTestsData.result[e]));
-                  }
-            }
+          $scope.isBusy = false;
+          if (patientTestsData.size === 0){
+            $scope.allDataLoaded = true;
+          }else {
+            $scope.encounters.length != 0 ? $scope.encounters.push.apply($scope.encounters, patientTestsData.result) :
+             $scope.encounters = patientTestsData.result;
+            $scope.nextStartIndex +=  patientTestsData.size;
 
-              if (patientTestsData.size !== 0) {
-                if ($scope.testLength==0)
-                {
-                  $scope.isBusy = false;
-                 loadMoreLabs();
-                }
-                else {
-                  $scope.isBusy = false;
-                }
-              }
-              else {
-                $scope.allDataLoaded = true;
-              }
+          }
+          buildDataTable();
+
         }
 
         function onFetchPatientTestsFailed(error) {
             $scope.experiencedLoadingError = true;
             $scope.isBusy = false;
         }
+
+      /**
+       * Functions to populate and define bootstrap data table
+       */
+
+      function buildDataTable() {
+          buildColumns();
+          buildTableControls();
+
+      }
+
+      function buildColumns() {
+        $scope.columns = [];
+        _.each($scope.labTestTags, function (header) {
+          //var visible =(header!=='location_uuid');
+          $scope.columns.push({
+            field: header.headers,
+            title: $filter('titlecase')(header.headers.toString().split('_').join(' ')),
+            align: 'center',
+            valign: 'center',
+            class: header.name === 'test_datetime' ? 'bst-table-min-width' : undefined,
+            sortable:true,
+            visible:true,
+            tooltip: true,
+            formatter: function (value, row, index) {
+              // console.log('this is the value ',value,row);
+              return cellFormatter(value, row, index, header);
+
+            }
+          });
+        });
+      }
+
+      function buildTableControls() {
+        $scope.bsTableControl = {
+          options: {
+            data: $scope.encounters,
+            rowStyle: function (row, index) {
+              return {classes: 'none'};
+            },
+            tooltip: true,
+            classes: 'table table-hover',
+            cache: false,
+            height: 550,
+            detailView: false,
+            //detailFormatter: detailFormatter,
+            striped: true,
+            selectableRows: true,
+            showFilter: true,
+            pagination: true,
+            pageSize: 20,
+            pageList: [5, 10, 25, 50, 100, 200],
+            search: true,
+            trimOnSearch: true,
+            singleSelect: false,
+            showColumns: true,
+            showRefresh: true,
+            showMultiSort: true,
+            showPaginationSwitch: true,
+            smartDisplay: true,
+            idField: 'patientUuid',
+            minimumCountColumns: 2,
+            clickToSelect: true,
+            showToggle: false,
+            maintainSelected: true,
+            showExport: true,
+            toolbar: '#toolbar',
+            toolbarAlign: 'left',
+            exportTypes: ['json', 'xml', 'csv', 'txt', 'png', 'sql', 'doc', 'excel', 'powerpoint', 'pdf'],
+            columns: $scope.columns,
+            exportOptions: {fileName: ''},
+            iconSize: undefined,
+            iconsPrefix: 'glyphicon', // glyphicon of fa (font awesome)
+            icons: {
+              paginationSwitchDown: 'glyphicon-chevron-down',
+              paginationSwitchUp: 'glyphicon-chevron-up',
+              refresh: 'glyphicon-refresh',
+              toggle: 'glyphicon-list-alt',
+              columns: 'glyphicon-th',
+              sort: 'glyphicon-sort',
+              plus: 'glyphicon-plus',
+              minus: 'glyphicon-minus',
+              detailOpen: 'glyphicon-plus',
+              detailClose: 'glyphicon-minus'
+            }
+          }
+        };
+      }
+
+
+      /**
+       * Converts null to empty string on display
+       */
+      function nullToEmptyString(value) {
+        if (value === null) return '<span class="text-success"></span>';
+        return value
+      }
+
+      /**
+       * Function to add button on each cell
+       */
+      function cellFormatter(value, row, index, header) {
+
+        if(header.name==='test_datetime') return '<div class="text-center" style="height:43px;!important;" ><span ' +
+          'class="">'+ $filter('date')(row.test_datetime, 'dd-MM-yyyy')+'</span></div>';
+
+        return ['<',
+          ''  +row.patient_uuid +'">' + nullToEmptyString(value)
+        ].join('');
+      }
+
     }
 
     function labsSummaryLink(scope, element, attrs, vm) {
