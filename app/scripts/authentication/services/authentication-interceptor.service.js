b8e1ae3c25ed09f54d5d4f56ba21d65366093252
vtuwei <vtuwei@ampath.or.ke>
2016-09-07 15:09:11 +0300

    APTS-145: Add Validation to prevent duplication of orders in EID (#572)

--- app/scripts/authentication/services/authentication-interceptor.service.js
+++ app/scripts/authentication/services/authentication-interceptor.service.js
@@ -5,29 +5,32 @@
   angular
     .module('app.authentication')
     .factory('authenticationErrorInterceptor', AuthenticationErrorInterceptor);
-  AuthenticationErrorInterceptor.$inject = ['$q', '$injector'];
-  var retries = 0,
-         waitBetweenErrors = 500,
-         maxRetries = 1;
-  function AuthenticationErrorInterceptor($q, $injector) {
-    function onResponseError(httpConfig) {
-      var $http = $injector.get('$http');
-      setTimeout(function() {
+
+    AuthenticationErrorInterceptor.$inject = ['$q', '$injector'];
+
+    var
+      retries = 0,
+      waitBetweenErrors = 500,
+      maxRetries = 1;
+
+    function AuthenticationErrorInterceptor($q, $injector) {
+
+      function onResponseError(httpConfig) {
+        var $http = $injector.get('$http');
         return $http(httpConfig);
-      }, waitBetweenErrors);
+      }
+
+      return {
+        responseError: function(response) {
+
+          if (response.status >= 403 && retries < maxRetries) {
+            retries++;
+            return onResponseError(response.config);
+          }
+
+          retries = 0;
+          return $q.reject(response);
+        },
+      };
     }
-
-    return {
-      responseError: function(response) {
-        if (response.status >= 403 && retries < maxRetries) {
-          retries++;
-          return onResponseError(response.config);
-        }
-
-        retries = 0;
-        return $q.reject(response);
-      },
-    };
-  }
-
 })();
