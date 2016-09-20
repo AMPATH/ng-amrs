e07d98c11bada85e102230a2beaf2aacab0ccf16
kwatuha <kwatuha@gmail.com>
2016-09-20 16:35:14 +0300

    AA-807:Adding ability to enter and exit a patient from a program workflow (#590)
    in POC

--- app/scripts/patient-dashboard/directives/patient-program-enrollment.directive.js
+++ app/scripts/patient-dashboard/directives/patient-program-enrollment.directive.js
@@ -13,21 +13,24 @@
 
         return patientProgramEnrollmentDefinition;
     }
-    PatientProgramEnrollmentCtrl.$inject = ['$rootScope', '$scope', '$stateParams', 'OpenmrsRestService', '$state', 'dialogs', '$timeout', 'ProgramEnrollmentResService', 'ProgramEnrollmentHelperService', '$uibModal'];
-    function PatientProgramEnrollmentCtrl($rootScope, $scope, $stateParams, OpenmrsRestService, $state, dialogs, $timeout, ProgramEnrollmentResService, EnrollmentHelperService, $uibModal) {
+    PatientProgramEnrollmentCtrl.$inject = ['$rootScope', '$scope', '$stateParams',
+        'OpenmrsRestService', '$state', 'dialogs', '$timeout', 'ProgramEnrollmentResService',
+        'ProgramEnrollmentHelperService', '$uibModal', 'moment'];
+    function PatientProgramEnrollmentCtrl($rootScope, $scope, $stateParams,
+        OpenmrsRestService, $state, dialogs, $timeout, ProgramEnrollmentResService,
+        EnrollmentHelperService, $uibModal, moment) {
        
         //variables
         $scope.showSuccessAlert = false;
         $scope.successAlert = '';
         $scope.currentDate = new Date();
         $scope.isBusy = false;
-        
+        $scope.saving = false;
+                
         //functions
         $scope.updateEnrollment = updateEnrollment;
-        $scope.setEnrolledDate = setEnrolledDate;
-        $scope.setcompletedDate = setcompletedDate;
         $scope.validateFormFields = validateFormFields;
-        $scope.validateDateInput = validateDateInput;           
+        $scope.validateDateInput = isDateValid;           
        
         //initialize 
         activate();
@@ -54,35 +57,25 @@
 
         }
 
-        function setEnrolledDate(date) {
-            $scope.enrolledDate = date;
-            $scope.hasError = false;
-            $scope.errorMessage = '';
-        }
-
-        function setcompletedDate(date) {
-            $scope.completedDate = date;
-            $scope.hasError = false;
-            $scope.errorMessage = '';
-
-        }
-
         function updateEnrollment() {
             var isFormValid = validateFormFields($scope.enrolledDate, $scope.completedDate);
             var enrollmentPayload = EnrollmentHelperService.createEnrollmentPayload($scope.selectedPatient, $scope.programUuid, $scope.enrolledDate, $scope.completedDate, $scope.enrollmentUuid);
 
             if (isFormValid == true && enrollmentPayload) {
                 $scope.isBusy = true;
+                $scope.saving = true;
                 ProgramEnrollmentResService.saveUpdateProgramEnrollment(enrollmentPayload,
                     function (response) {
                         console.log('Patient Enrolled Succeffully' + JSON.stringify(response));
                         $scope.$emit('ProgramEnrollmentUpdated', response);
                         $scope.isBusy = false;
+                        $scope.saving = false;
                     },
                     function (error) {
                         $scope.hasError = true;
                         $scope.errorMessage = 'Error Occurred while Enrolling Patient';
                         $scope.isBusy = false;
+                        $scope.saving = false;
                         console.log('Error Occurred while Enrolling Patient' + JSON.stringify(error));
                     }
 
@@ -90,50 +83,44 @@
             }
         }
 
-        function validateDateInput(inputValue, errorMessage) {
-            var customDate, isValid;
-            if (angular.isDefined(inputValue) && inputValue !== '') {
-                customDate = new Date(inputValue);
-                isValid = !isNaN(customDate);
+        function isDateValid(inputValue) {
+            return moment(inputValue).isValid();;
+        }
 
-                if (!isValid) {
-                    $scope.hasError = true;
-                    $scope.errorMessage = errorMessage;
-                }
-            }
-            return isValid;
+
+        function setErroMessage(message) {
+
+            $scope.hasError = true;
+            $scope.errorMessage = message;
+        }
+
+        function isNullOrUndefined(val) {
+            return val === null || val === undefined;
         }
 
         function validateFormFields(enrolledDate, completedDate) {
-            $scope.hasError = false;
-            $scope.errorMessage = '';
-            var isValidDate;
-            if (angular.isDefined(enrolledDate) && enrolledDate !== '') {
-                isValidDate = validateDateInput(enrolledDate, 'Invalid Date Enrolled Value')
+
+            if (isNullOrUndefined(enrolledDate)) {
+                setErroMessage('Date Enrolled is required.');
+                return false;
             }
 
-            if (angular.isDefined(completedDate) && completedDate !== '') {
-                isValidDate = validateDateInput(completedDate, 'Invalid Date Completed Value')
+            if (!isDateValid(enrolledDate)) {
+                setErroMessage('Date Enrolled should be a valid date.');
+                return false;
             }
-            
-            //when a user attempts to provide completion date without providing date enrolled
-            if (completedDate && (enrolledDate == '' || !angular.isDefined(enrolledDate)) && isValidDate == true) {
-                $scope.hasError = true;
-                $scope.errorMessage = 'Date Enrolled is Required';
-                isValidDate = false;
+
+            if (!isNullOrUndefined(completedDate) && !isDateValid(completedDate)) {
+                setErroMessage('Date Completed should be a valid date.');
+                return false;
             }
-            
-            //when a user attempts to provide completion date greater than date enrolled
-            if ((completedDate && enrolledDate) && isValidDate == true) {
-                if (enrolledDate > completedDate) {
-                    $scope.hasError = true;
-                    $scope.errorMessage = 'Date completed should be greater than Date Enrolled';
-                    isValidDate = false;
-                } else {
-                    isValidDate = true;
-                }
+
+            if (isDateValid(completedDate) && !moment(completedDate).isAfter(enrolledDate)) {
+                setErroMessage('Date Completed should be after Date Enrolled');
+                return false;
             }
-            return isValidDate;
+
+            return true;
         }
     }
 
